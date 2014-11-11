d31785d68daad3958a4e7a3deec0e224dbdae171
Bobby Rogers <bobby@blueacorn.com>
2014-11-11 16:45:51 -0500

    Attempting to get parameter switching to work

--- app.js
+++ app.js
@@ -7,10 +7,17 @@ program
     .option('--list-profiles', 'List configured site profiles')
     .option('--run-profile [profile name]', 'Run given profile')
     .option('--full-dump', 'Used with --run-profile, performs a full DB dump')
-    .option('--selective-dump', 'Used with --run-profile, performs a selective table dump with default tables excluded');
+    .option('--selective-dump', 'Used with --run-profile, performs a selective table dump with default tables excluded')
+    .parse(process.argv);
+
 
 function MagentoDbTools(){
-    webServer.startServer();
+    if (process.startServer) {
+        webServer.startServer();
+    }
+    else if (process.runProfile) {
+        console.log(process.runProfile);
+    }
 }
 
 new MagentoDbTools();
